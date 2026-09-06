878472b43d1d3a1a2b31ead1ac547387a24da8c1
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 10:25:47 +0800

    Export clone splice module

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod file_append_batch;
 pub mod file_clone_append;
 pub mod file_clone_insert;
 pub mod file_clone_replace;
+pub mod file_clone_splice;
 pub mod file_collapse;
 pub mod file_copy_range;
 pub mod file_data;
