a611fbbc7330bdac8a7049d30941aca2f7816c7a
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-08 02:30:13 +0800

    Export pathname clone-append API

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,7 @@ pub mod journal_codec;
 pub mod journal_region;
 pub mod metadata_tx;
 pub mod path_append;
+pub mod path_clone_append;
 pub mod path_copy_range;
 pub mod path_exchange;
 pub mod path_hard_link;
