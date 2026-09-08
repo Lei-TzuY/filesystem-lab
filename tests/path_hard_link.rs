9d8c37e716a13335516f3735db37011486c9b27a
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-09 04:27:24 +0800

    Apply canonical rustfmt layout

--- tests/path_hard_link.rs
+++ tests/path_hard_link.rs
@@ -94,8 +94,12 @@ fn creates_regular_file_hard_link_through_source_and_parent_symlinks() {
 #[test]
 fn creates_symlink_hard_link_without_following_final_source() {
     let (mut device, superblock) = setup();
-    let source_inode =
-        resolve_path_without_following_final_symlink(&mut device, &superblock, "/file_alias").unwrap();
+    let source_inode = resolve_path_without_following_final_symlink(
+        &mut device,
+        &superblock,
+        "/file_alias",
+    )
+    .unwrap();
     let allocator_before = load_allocator(&mut device, &superblock).unwrap();
     let inodes_before = load_inode_table(&mut device, &superblock).unwrap();
 
@@ -111,7 +115,7 @@ fn creates_symlink_hard_link_without_following_final_source() {
         resolve_path_without_following_final_symlink(
             &mut device,
             &superblock,
-            "/dir/symlink_link"
+            "/dir/symlink_link",
         )
         .unwrap(),
         source_inode
@@ -295,7 +299,8 @@ fn every_pathname_symlink_hard_link_crash_point_recovers_old_or_complete_new_sta
         let allocator_before = load_allocator(&mut device, &superblock).unwrap();
         let inodes_before = load_inode_table(&mut device, &superblock).unwrap();
         let entries_before = load_directory_table(&mut device, &superblock).unwrap();
-        let target_before = read_symlink_at_path(&mut device, &superblock, "/file_alias").unwrap();
+        let target_before =
+            read_symlink_at_path(&mut device, &superblock, "/file_alias").unwrap();
 
         device.arm(Some(crash_at));
         assert_eq!(
