3d9650397b11b837bd04785995e76a60b706722a
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-12 04:24:23 +0800

    Export pathname whole-block reads

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,7 @@ pub mod path_create;
 pub mod path_directory;
 pub mod path_directory_rename_overwrite;
 pub mod path_exchange;
+pub mod path_file_read;
 pub mod path_grow;
 pub mod path_hard_link;
 pub mod path_insert;
