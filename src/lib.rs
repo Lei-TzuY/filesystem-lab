36526f2ccb5c7f71fa9d64b8eab331f0ff10bb50
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-13 02:26:29 +0800

    Export contiguous pathname clone-create

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,7 @@ pub mod path_clone_replace;
 pub mod path_clone_splice;
 pub mod path_collapse;
 pub mod path_contiguous_append;
+pub mod path_contiguous_clone_create;
 pub mod path_contiguous_create;
 pub mod path_contiguous_defrag;
 pub mod path_contiguous_insert;
