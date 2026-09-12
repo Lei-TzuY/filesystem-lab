99a5dc898aa74d75d3cc02bb6123e34e9497019b
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-13 01:27:19 +0800

    Export pathname contiguous defragmentation

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,7 @@ pub mod path_clone_splice;
 pub mod path_collapse;
 pub mod path_contiguous_append;
 pub mod path_contiguous_create;
+pub mod path_contiguous_defrag;
 pub mod path_contiguous_insert;
 pub mod path_contiguous_replace;
 pub mod path_copy_range;
