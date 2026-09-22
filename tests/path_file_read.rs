2570d9271d8bb6b6660c12df3da6709ed7b17bb2
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 04:45:31 +0800

    Migrate byte EOF literals in pathname tests

--- tests/path_file_read.rs
+++ tests/path_file_read.rs
@@ -61,6 +61,8 @@ fn initialize_root(device: &mut MemoryDevice, superblock: &Superblock) {
             id: 1,
             kind: InodeKind::Directory,
             blocks: Vec::new(),
+        
+            byte_len: 0,
         }],
     )
     .unwrap();
