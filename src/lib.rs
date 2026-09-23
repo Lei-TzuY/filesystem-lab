95d3e0a24ac66b1ddc06774a026fa7b8ee0e1599
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 09:23:01 +0800

    Export recursive subtree removal transaction

--- src/lib.rs
+++ src/lib.rs
@@ -99,6 +99,7 @@ pub mod path_whole_exchange;
 pub mod path_whole_transfer_replace;
 pub mod path_zero_range;
 pub mod recovery;
+pub mod recursive_remove_tx;
 pub mod recovery_projection;
 pub mod rename_exchange_tx;
 mod rename_overwrite_nondirectory_tx;
